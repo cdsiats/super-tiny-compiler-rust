e7d6683798d8ff5e6ed01f478675f2a47ad67e91
cdsiats <118641404+cdsiats@users.noreply.github.com>
2025-05-25 19:41:24 +0800

    feat: created base lexer implementation

---
+++ src/lexer.rs
@@ -0,0 +1,39 @@
+use std::char;
+
+use crate::tokens::Token;
+
+pub struct Lexer {
+    pub source: String,
+    pub current: usize
+}
+
+impl Lexer {
+    pub fn new(input: String) -> Self {
+        Lexer {
+            source: input,
+            current: 0
+        }
+    }
+
+    pub fn tokenize(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+        
+        while self.current < self.source.len() {
+            let char = &self.source.chars().nth(self.current).unwrap();
+
+            match char {
+                '(' => {
+                    tokens.push(Token { 
+                        kind: "paren".to_string(), 
+                        value: char.to_string(),
+                    });
+                    self.current += 1;
+                    continue;
+                },
+                _ => todo!("Handle catch all.")
+            }
+        }
+
+        tokens
+    }
+}
